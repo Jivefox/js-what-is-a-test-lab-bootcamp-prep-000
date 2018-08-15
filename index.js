70ba31444380f957104380968f8414275ac1449a
Cliff Jurgensen <jivefox.002@gmail.com>
2018-08-15 01:30:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 74
-if (74 < 40) {
-  console.log('74')
-}
-else {
-  console.log('74')
-}
 var message = ` is ${height} inches tall`
 
 module.exports = {name, height, message}
